87f45dad562402f48fbe3b4ebff456d4f481ed93
Christopher Chapline <cachapline8@gmail.com>
2021-04-28 01:13:55 -0600

    Refined terrain generation
    Now includes probabalistic spreading of water terrain, drenching of land
    surrounded by water, and the addition of coastlines to water-bordering
    terrain.

--- src/four_nations/model/map/game_map.clj
+++ src/four_nations/model/map/game_map.clj
@@ -4,12 +4,15 @@
 (defrecord GameTile [terrain-type raw-value x y])
 (defrecord GameMap [game-map height width])
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Helper functions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn print-map
   [game-map]
   (doseq [row game-map]
     (doseq [cell row]
       (print (condp = (:terrain-type cell)
-               :plains "G"
+               :land "G"
                :water "~"
                :mountain "^"
                :coast "."
@@ -17,17 +20,23 @@
     (println)))
 
 (defn average-map-value
+  "Given a noise map, calculates the average value of all of the cells within that noise map."
   [noise]
   (let [all-values (flatten noise)]
     (quot (apply + all-values)
           (count all-values))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Initial map tile creation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn determine-cell-terrain-type
+  "Determines if a cell should be land, water, or mountain based on the cell value's relation to
+   the average value of the game map."
   [cell-value average-value]
   (cond
-    (> cell-value (+ average-value 20)) :mountain
+    (> cell-value (+ average-value 15)) :mountain
     (< cell-value average-value) :water
-    :else :plains))
+    :else :land))
 
 (defn cell-value->initial-game-tile
   "Given the value of a particular cell, builds a game tile that includes the basic terrain type."
@@ -35,15 +44,53 @@
   (let [terrain-type (determine-cell-terrain-type cell-value average-value)]
     (->GameTile terrain-type cell-value x y)))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Building the game map and adding variety to the terrain
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn spread-water
-  [{:keys [game-map height width]} average-value]
+  "Given a game map and spread chance, for every land tile within the game that is near water, the
+   spread chance determines how likely it is that the water will spread to that tile."
+  [{:keys [game-map height width]} spread-chance]
+  (-> (fn [tile x y]
+        (let [neighbors (utils/coordinates->neighbors height width game-map [x y])]
+          (if (and (-> tile :terrain-type (= :land))
+                   (->> neighbors
+                        (map :terrain-type)
+                        (some (partial = :water)))
+                   (< (rand) spread-chance))
+            (assoc tile :terrain-type :water)
+            tile)))
+      (utils/two-dimensional-map game-map height width)
+      (->GameMap height width)))
+
+(defn drench-surrounded-land
+  "Given a game map, finds any land tiles where all of its cardinal neighbors are water and sets
+   those tiles to also be water. This prevents tiny, nonsensical islands."
+  [{:keys [game-map height width]}]
   (-> (fn [tile x y]
         (let [neighbors (utils/coordinates->neighbors height width game-map [x y] false)]
-          tile))
+          (if (and (->> neighbors (map :terrain-type) (some (partial = :water)))
+                   (-> tile :terrain-type (= :land)))
+            (assoc tile :terrain-type :water)
+            tile)))
+      (utils/two-dimensional-map game-map height width)
+      (->GameMap height width)))
+
+(defn add-coastline
+  "Given a map, changes the terrain type of any land tiles that are near water to be coastline."
+  [{:keys [game-map height width]}]
+  (-> (fn [tile x y]
+        (let [neighbors (utils/coordinates->neighbors height width game-map [x y])]
+          (if (and (->> neighbors (map :terrain-type) (some (partial = :water)))
+                   (-> tile :terrain-type (= :land)))
+            (assoc tile :terrain-type :coast)
+            tile)))
       (utils/two-dimensional-map game-map height width)
       (->GameMap height width)))
 
 (defn noise-map->basic-game-map
+  "Given the noise map and the average value of cells across the map, builds basic game map with
+   simple terrain selection."
   [{:keys [noise height width]} average-value]
   (-> (fn [cell-value x y]
         (-> cell-value
@@ -53,19 +100,30 @@
       (->GameMap height width)))
 
 (defn noise-map->game-map
-  [noise-map]
+  "Given a generated and smoothed noisemap, generates a game map with terrain added."
+  [noise-map & {:keys [water-spread-chance]
+                :or {water-spread-chance 0.05}}]
   (let [average-value (-> noise-map :noise average-map-value)]
-    (noise-map->basic-game-map noise-map average-value)))
+    (-> noise-map
+        (noise-map->basic-game-map average-value)
+        (spread-water water-spread-chance)
+        drench-surrounded-land
+        add-coastline
+        )))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Rich comments for experimenting :)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (comment
   (require '[four-nations.model.map.noise-map :as nm])
   (use 'clojure.pprint)
   (let [height 50
         width 250
-        smoothing-passes 20]
-    (->> (nm/generate-noisemap height width smoothing-passes)
-         noise-map->game-map
-         :game-map
-         print-map)
+        water-spread-chance 0.01
+        smoothing-passes 15]
+    (-> (nm/generate-noisemap height width smoothing-passes)
+        (noise-map->game-map :water-spread-chance water-spread-chance)
+        :game-map
+        print-map)
     )
   )
