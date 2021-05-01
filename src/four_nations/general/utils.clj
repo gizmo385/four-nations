f4c8320066fdb1e94e36551b643629ddcdc02dfa
Christopher Chapline <cachapline8@gmail.com>
2021-05-01 02:15:47 -0600

    Initial version of on-map resources like trees

--- src/four_nations/general/utils.clj
+++ src/four_nations/general/utils.clj
@@ -1,4 +1,12 @@
-(ns four-nations.general.utils)
+(ns four-nations.general.utils
+  (:require
+    [clojure.edn :as edn]
+    [clojure.java.io :as io]))
+
+(defn load-edn-resource
+  "Given a filename, reads that filename from the resources/ directory and loads its EDN contents."
+  [resource-filename]
+  (-> resource-filename io/resource slurp edn/read-string))
 
 (defn n-times
   "Given a function f, an initial value, and a number n, performs n calls of f. The initial value is"
---
+++ src/four_nations/model/map/resources.clj
@@ -0,0 +1,69 @@
+(ns four-nations.model.map.resources
+  (:require
+    [four-nations.general.utils :as utils]
+    [random-seed.core :as rs]))
+
+(defrecord Resource
+  [resource-name text-symbol spawn-predicate])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Validating resource definitions
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn valid-base-resource?
+  "Ensures that the resource has all of the basic required fields."
+  [resource]
+  (and (:name resource) (:symbol resource) (:predicate resource)))
+
+(defmulti valid-resource-definition?
+  "The validation of a resource definition is done based on what kind of predicate the resource
+   defines."
+  (fn [resource] (:predicate resource)))
+
+(defmethod valid-resource-definition? :default [& _] false)
+
+(defmethod valid-resource-definition? :spawn-on-terrain-types
+  [{:keys [spawn-chance terrain-types] :as resource}]
+  (and (valid-base-resource? resource) spawn-chance terrain-types))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Building spawn predicates
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmulti resource-definition->spawn-predicate
+  "For a particular resource, builds the spawn predicate function that takes a tile and determines
+   if the resource will spawn there."
+  (fn [resource] (:predicate resource)))
+
+(defmethod resource-definition->spawn-predicate :default [& _]
+  ;; If a resource is declared and the predicate is unknown, we'll ignore it. This should
+  ;; get caught in the validation steps.
+  (constantly false))
+
+(defmethod resource-definition->spawn-predicate :spawn-on-terrain-types
+  [{:keys [terrain-types spawn-chance] :as resource}]
+  (fn [tile]
+    (let [rand-value (rs/rand)]
+      (and (< rand-value spawn-chance)
+           (some #{(:terrain-type tile)} terrain-types)))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Parsing the resources EDN file
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn resource-definition->resource
+  "Given an EDN resource definiton, validations that definition and builds the Resource record. This
+   resource record will include the tile-based spawn predicate."
+  [resource-definition]
+  (if (valid-resource-definition? resource-definition)
+    (let [spawn-predicate (resource-definition->spawn-predicate resource-definition)]
+      (->Resource (:name resource-definition) (:symbol resource-definition) spawn-predicate))
+    (throw (ex-info "Invalid resource definition!" resource-definition))))
+
+(defn load-resource-definitions
+  "Given a file with EDN resource definitions, returns resource records."
+  [filename]
+  (->> filename
+       utils/load-edn-resource
+       (map resource-definition->resource)))
+
+(comment
+  (load-resource-definitions "resources.edn")
+  )
