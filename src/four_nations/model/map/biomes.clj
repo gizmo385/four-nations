128f6100494724c5944ab4940190affbf93849a8
Christopher Chapline <cachapline8@gmail.com>
2021-05-05 00:13:20 -0600

    Add later ref for map biomes

---
+++ src/four_nations/model/map/biomes.clj
@@ -0,0 +1,6 @@
+(ns four-nations.model.map.biomes
+  "The idea for this is still under development. It would likely involve subdividing the game map
+   and then assigning biomes and growing those biomes.
+
+   For the actual implementation, potentially reference this StackOverflow post:
+    https://stackoverflow.com/questions/3397972/divide-grid-2d-array-into-random-shaped-parts")
