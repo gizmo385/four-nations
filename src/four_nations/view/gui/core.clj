3f07960eed08a5032b93149c9c3005c4a29b4a68
Christopher Chapline <cachapline8@gmail.com>
2021-05-06 00:28:28 -0600

    Working on map scrolling/zooming

--- src/four_nations/view/gui/core.clj
+++ src/four_nations/view/gui/core.clj
@@ -1,40 +1,54 @@
 (ns four-nations.view.gui.core
   (:require
     [cljfx.api :as fx]
+    [clojure.core.cache :as cache]
+    [clojure.edn :as edn]
     [four-nations.model.map :as m]
     [four-nations.general.utils :as utils]
     [four-nations.model.map.utils :as map-utils]
     [four-nations.view.gui.images :as images])
   (:import
     [javafx.application Application Platform]
+    [javafx.stage Screen]
     [javafx.scene.canvas Canvas]
     [javafx.scene.paint Color]
     [javafx.scene Group Scene]
-    [javafx.scene.shape Rectangle]))
+    [javafx.scene.shape Rectangle]
+    [javafx.scene.input KeyCode]))
 
-(def terrain-type->color
-  {:water Color/BLUE
-   :land Color/GREEN
-   :coast Color/BEIGE
-   :mountain Color/GREY})
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; State and constants
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defrecord MapViewPort [x y])
 
+(def *state
+  (atom (fx/create-context
+          {:viewport (->MapViewPort 0 0)
+           :tile-size 32
+           :height 500
+           :width 500
+           :game-map nil}
+          cache/lru-cache-factory)))
+
+;; TODO: Maybe in a config file?
 (def terrain-type->image
   {:water "images/water.png"
    :land "images/grass.png"
    :mountain "images/mountain.png"
    :coast "images/coast.png"})
 
-(defn draw-rectangle
-  [canvas tile x y tile-size]
-  (let [graphics (.getGraphicsContext2D canvas)]
-    (doto graphics
-      (.setFill (-> tile :terrain-type terrain-type->color))
-      (.fillRect x y tile-size tile-size))
-    (when (> tile-size 1)
-      (doto graphics
-        (.setStroke Color/BLACK)
-        (.setLineWidth 1)
-        (.strokeRect x y tile-size tile-size)))))
+
+(defmulti event-handler :event/type)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; The Canvas element
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn clear-canvas!
+  [canvas]
+  (let [graphics (.getGraphicsContext2D canvas)
+        height (.getHeight canvas)
+        width (.getWidth canvas)]
+    (.clearRect graphics 0 0 width height)))
 
 (defn draw-image
   "For a tile at a particular spot on the graphics canvas, draw images on that position."
@@ -47,48 +61,113 @@
       (doto graphics
         (.drawImage (images/load-image resource-image) x y tile-size tile-size)))))
 
-(defn clear-canvas!
-  [canvas]
-  (let [graphics (.getGraphicsContext2D canvas)
-        height (.getHeight canvas)
-        width (.getWidth canvas)]
-    (.clearRect graphics 0 0 width height)))
-
 (defn draw-map
-  [canvas game-map tile-size]
+  "Given a canvas and information about the game map, draws the map onto the canvas."
+  [canvas game-map tile-size viewport]
   (clear-canvas! canvas)
-  (doseq [x (range (:width game-map))]
-    (doseq [y (range (:height game-map))]
-      (let [draw-x (* tile-size x)
-            draw-y (* tile-size y)
-            tile (map-utils/get-cell (:game-map game-map) x y)
-            tile-image (-> tile :terrain-type terrain-type->image images/load-image)]
-        (draw-image canvas tile draw-x draw-y tile-size)))))
+  (let [canvas-height (.getHeight canvas)
+        canvas-width (.getWidth canvas)
+        horizontal-viewport-size (quot canvas-width tile-size)
+        vertical-viewport-size (quot canvas-height tile-size)]
+    (doseq [x (range (:x viewport) (min (+ (:x viewport) horizontal-viewport-size) canvas-width))]
+      (doseq [y (range (:y viewport) (min (+ (:y viewport) vertical-viewport-size) canvas-height))]
+        (let [draw-x (* tile-size x)
+              draw-y (* tile-size y)]
+          (when-let [tile (map-utils/get-cell (:game-map game-map) x y)]
+            (draw-image canvas tile draw-x draw-y tile-size)))))))
 
 (defn canvas-map
-  "for a game map, draw the game map in the canvas"
-  [{:keys [width height tile-size game-map]}]
-  {:fx/type :canvas
-   :width width
-   :height height
-   :draw (fn [^Canvas canvas] (draw-map canvas game-map tile-size))})
+  "Defines a JavaFX canvas component that draws the game map."
+  [{:keys [fx/context max-height max-width]}]
+  (let [game-map (fx/sub-val context :game-map)
+        tile-size (fx/sub-val context :tile-size)
+        viewport (fx/sub-val context :viewport)]
+    {:fx/type :canvas
+     :height max-width
+     :width max-height
+     :draw (fn [^Canvas canvas]
+             (draw-map canvas game-map tile-size viewport))}))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Changing the tile size
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defmethod event-handler ::update-tile-size
+  [{:keys [fx/event]}]
+  (swap! *state fx/swap-context assoc :tile-size (int (Math/floor event))))
+
+(defn tile-size-slider
+  [{:keys [height width fx/context]}]
+  (let [tile-size (fx/sub-val context :tile-size)
+        viewport (fx/sub-val context :viewport)]
+    {:fx/type :slider
+     :min 0
+     :max 64
+     :value tile-size
+     :on-value-changed {:event/type ::update-tile-size}}))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; Root application definition
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn translate-viewport!
+  [{:keys [x y] :as viewport} delta-x delta-y map-height map-width]
+  (let [new-x (max 0 (min (+ x delta-x) map-width))
+        new-y (max 0 (min (+ y delta-y) map-height))
+        new-viewport (assoc viewport :x new-x :y new-y)]
+    (println :old viewport :new new-viewport)
+    (swap! *state fx/swap-context assoc :viewport new-viewport)))
+
+(defmethod event-handler ::key-pressed
+  [{:keys [fx/event viewport map-height map-width]}]
+  (let [key-code (.getCode event)]
+    (condp = key-code
+      KeyCode/W (translate-viewport! viewport 0 -1 map-height map-width)
+      KeyCode/A (translate-viewport! viewport -1 0 map-height map-width)
+      KeyCode/S (translate-viewport! viewport 0 1 map-height map-width)
+      KeyCode/D (translate-viewport! viewport 1 0 map-height map-width)
+      nil)))
+
+(defn root
+  [{:keys [fx/context map-height map-width]}]
+  (println :map-height map-height :map-width map-width)
+  (let [bounds (-> (Screen/getPrimary) .getBounds)
+        max-height (.getHeight bounds)
+        max-width (.getWidth bounds)
+        viewport (fx/sub-val context :viewport)]
+    {:fx/type :stage
+     :showing true
+     :title "Game Map"
+     :max-height max-height
+     :max-width max-width
+     :scene {:fx/type :scene
+             :on-key-pressed {:event/type ::key-pressed
+                              :viewport viewport
+                              :map-height map-height
+                              :map-width map-width}
+             :root {:fx/type :v-box
+                    :padding 10
+                    :spacing 10
+                    :children [{:fx/type tile-size-slider}
+                               {:fx/type canvas-map
+                                :max-height max-height
+                                :max-width max-width}]}}}))
+
+(defn build-renderer
+  [map-height map-width]
+  (fx/create-renderer
+    :middleware (comp
+                  fx/wrap-context-desc
+                  (fx/wrap-map-desc (fn [_] {:fx/type root
+                                             :map-height map-height
+                                             :map-width map-width})))
+    :opts {:fx.opt/map-event-handler event-handler
+           :fx.opt/type->lifecycle #(or (fx/keyword->lifecycle %)
+                                        (fx/fn->lifecycle-with-context %))}))
 
 (defn -main [& args]
   (Platform/setImplicitExit true)
-  (let [map-height 140
-        map-width 230
-        game-map (m/build-map map-height map-width)]
-    (fx/on-fx-thread
-      (fx/create-component
-        {:fx/type :stage
-         :showing true
-         :title "Game Map"
-         :scene {:fx/type :scene
-                 :root {:fx/type :v-box
-                        :padding 10
-                        :spacing 10
-                        :children [{:fx/type canvas-map
-                                    :width 2560
-                                    :height 1440
-                                    :game-map game-map
-                                    :tile-size 10}]}}}))))
+  (let [map-height 50
+        map-width 50
+        game-map (m/build-map map-height map-width)
+        view-renderer (build-renderer map-height map-width)]
+    (swap! *state fx/swap-context assoc :game-map game-map)
+    (fx/mount-renderer *state view-renderer)))
