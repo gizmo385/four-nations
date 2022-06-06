0088a9216ca4a2d204fd37b758d325831560cb65
Christopher Chapline <gizmo385@users.noreply.github.com>
2022-06-06 01:12:22 -0600

    Tile images

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/clojurescript "1.10.773"]
                  [org.clojure/core.async  "0.4.500"]
-                 [reagent "0.10.0"]]
+                 [reagent "0.10.0"]
+                 [org.clojars.frozenlock/reagent-modals "0.2.8"]]
 
   :plugins [[lein-figwheel "0.5.20"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
